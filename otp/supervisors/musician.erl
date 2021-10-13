b5aa6b2a9e718252eea68f793cc0edb8d27716c1
Andronikus <tiagoafroque@gmail.com>
2021-10-13 21:40:25 +0100

    add get names function

--- otp/supervisors/musician.erl
+++ otp/supervisors/musician.erl
@@ -2,7 +2,7 @@
 -behaviour(gen_server).
 
 -export([start_link/2, stop/1]).
--export([init/1, handle_call/3]).
+-export([init/1, handle_call/3, handle_cast/2]).
 
 -record(state, {name="", role, skill=good}).
 
@@ -26,5 +26,14 @@ handle_call(_Message, _From, _State) ->
 handle_cast(_Message, _State) ->
   erlang:error("Not implemented").
 
+
+%%% Private APIs
 pick_name() ->
-  "James Rock".
+  lists:nth(rand:uniform(10), first_names()) ++ " " ++ lists:nth(rand:uniform(10), last_names()).
+
+
+first_names() ->
+  ["Valerie", "Arnold", "Carlos", "Dorothy", "Keesha", "Phoebe", "Ralphie", "Tim", "Wanda", "Janet"].
+
+last_names() ->
+  ["Frizzle", "Perlstein", "Ramon", "Ann", "Franklin", "Terese", "Tennelli", "Jamal", "Li", "Perlstein"].
